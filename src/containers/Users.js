08ba947933a85527b346fd84366997ea0d6b0a8d
Paige Niedringhaus <paigen11@gmail.com>
2020-01-08 17:30:52 -0500

    webpack custom config

---
+++ src/containers/Users.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+
+class Users extends Component {
+  render() {
+    return (
+      <div>
+        <h1>The Users</h1>
+        <p>Awesome users on board this course.</p>
+      </div>
+
+    )
+  }
+}
+
+export default Users;
